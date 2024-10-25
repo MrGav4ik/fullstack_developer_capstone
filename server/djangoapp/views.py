98d840ceacc3cbf2eda77c9fe0376d98ea8a674d
mrgav4ik <vitalik.super682@gmail.com>
2024-10-25 20:54:15 +0200

    linted

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if (count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
@@ -75,20 +75,20 @@ def registration(request):
 
     if not username_exist:
         user = User.objects.create_user(username=username,
-        first_name=first_name,
-        last_name=last_name, password=password, email=email)
+            first_name=first_name,
+            last_name=last_name, password=password, email=email)
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
 
     data = {"userName": username, "error": "Already Registered"}
     return JsonResponse(data)
-    
+
 
 # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if (state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/" + state
@@ -98,7 +98,7 @@ def get_dealerships(request, state="All"):
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
-    if(dealer_id):
+    if (dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
@@ -111,7 +111,7 @@ def get_dealer_reviews(request, dealer_id):
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
-    if(dealer_id):
+    if (dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status": 200, "dealer": dealership})
@@ -121,12 +121,11 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if(request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            response = post_review(data)
             return JsonResponse({"status": 200})
         except:
             return JsonResponse({"status": 401,
                                 "message": "Error in posting review"})
-    return JsonResponse({"status": 403, "message": "Unauthorized"}) 
+    return JsonResponse({"status": 403, "message": "Unauthorized"})
