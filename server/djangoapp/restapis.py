2f7a3b810b8f5571667eddfc137c5eeeaf510baf
mrgav4ik <vitalik.super682@gmail.com>
2024-10-25 21:20:50 +0200

    linted

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,12 +10,13 @@ sentiment_analyzer_url = os.getenv(
     default="https://sentianalyzer.1ngo4uiwbgna.us-south.codeengine.appdomain.cloud/"
 )
 
+
 def get_request(endpoint, **kwargs):
     params = ""
     if kwargs:
         for key, value in kwargs.items():
             params += f"{key}={value}&"
-    
+
     request_url = f"{backend_url}{endpoint}?{params}"
     print(f"GET from {request_url}")
 
@@ -25,8 +26,10 @@ def get_request(endpoint, **kwargs):
     except Exception as e:
         print(f"Network exception occurred: {e}")
 
+
 def analyze_review_sentiments(text):
     request_url = f"{sentiment_analyzer_url}analyze/{text}"
+    
     try:
         response = requests.get(request_url)
         return response.json()
@@ -34,8 +37,10 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 def post_review(data_dict):
     request_url = f"{backend_url}/insert_review"
+    
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
