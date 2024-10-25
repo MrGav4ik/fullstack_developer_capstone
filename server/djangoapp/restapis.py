7a32c0831f0f0609f4d60b1896322ecaed208595
mrgav4ik <vitalik.super682@gmail.com>
2024-10-25 08:57:23 +0200

    updated dynamic pages

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -9,7 +9,7 @@ backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="https://sentianalyzer.1nfbz77zopzm.us-south.codeengine.appdomain.cloud/")
 
 def get_request(endpoint, **kwargs):
     # Add code for get requests to back end
@@ -31,7 +31,8 @@ def get_request(endpoint, **kwargs):
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
-        response.requests.get(request_url)
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
         return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
